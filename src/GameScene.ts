c100ba8c35fbfad61022ad6caa6a91b1e63ee0cb
Jasmine Humbert <jasmine@humbert.dev>
2020-04-16 00:06:17 -0700

    [add] random colored pixels

--- src/GameScene.ts
+++ src/GameScene.ts
@@ -10,11 +10,20 @@ export class GameScene extends Phaser.Scene {
   }
 
   create (this: Phaser.Scene) {
-    this.add.image(400, 300, 'sky')
-    this.add.text(0, 150, 'Hello Typescript', {
-      color: 'red',
-      fontSize: '30px',
-      align: 'center'
-    })
+
+    for (var y = 0; y < 6; y++) {
+      for (var x = 0; x < 8; x++) {
+        const color = Phaser.Display.Color.RandomRGB()
+        const pixel = new Phaser.GameObjects.Rectangle(
+          this,
+          x * 100 + 50,
+          y * 100 + 50,
+          100,
+          100,
+          color.color
+        )
+        this.add.existing(pixel)
+      }
+    }
   }
 }
